54bbf9b35fb81d692da487a3e8dd83d3a1013602
zachary <zachrritchey@gmail.com>
2016-11-01 22:18:44 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String ss[]={"Bill", "Mary", "Lee", "Agnes", "Alfred", "Thomas", "Alvin", "Bernard", "Ezra", "Herman"};
+       Arrays.sort(ss);
+        for(String j:ss)
+            System.out.println(j);
+    }
+}
+
